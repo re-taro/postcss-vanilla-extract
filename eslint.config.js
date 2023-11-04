74d9fe80c7eae490777607625c010ae3e7817d6c
Rintaro Itokawa <me@re-taro.dev>
2023-11-04 18:35:58 +0900

    feat: initialize commit

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import { re_taro } from "@re-taro/eslint-config";
+
+export default re_taro(
+  {
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+  },
+  {
+    ignores: ["rollup.config.mjs", "vitest.config.ts"],
+  }
+);
---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+import pkg from "./package.json" assert { type: "json" };
+
+const externals = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
+];
+
+/** @type {import('rollup').RollupOptions} */
+const options = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: false,
+    },
+  ],
+  external: (id) => externals.some((d) => id.startsWith(d)),
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      outDir: ".",
+      declaration: true,
+    }),
+    terser(),
+  ],
+};
+
+export default options;
