09ec698efee032a7da29c25e16f6de2f6db575ac
Rintaro Itokawa <me@re-taro.dev>
2023-11-07 00:14:25 +0900

    feat: add utilities for testing

---
+++ packages/postcss-vanilla-extract/src/utils.ts
@@ -0,0 +1,16 @@
+import { parse as babelParse } from "@babel/parser";
+import type { File } from "@babel/types";
+
+/**
+ * Parses some javascript/typescript via babel
+ * @param {string} source Source to parse
+ * @return {File}
+ */
+export function parseScript(source: string): File {
+  const ast = babelParse(source, {
+    sourceType: "unambiguous",
+    ranges: true,
+  });
+
+  return ast;
+}
