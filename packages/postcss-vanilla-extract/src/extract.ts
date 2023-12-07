0463b3eae8a26b059df5ed9071336787faf04c24
Rintaro Itokawa <me@re-taro.dev>
2023-12-07 22:16:22 +0900

    build: setup prettier

--- packages/postcss-vanilla-extract/src/extract.ts
+++ packages/postcss-vanilla-extract/src/extract.ts
@@ -8,90 +8,92 @@ import type { SyntaxOptions } from "./types";
 
 /**
  * Determines if a given tag is one of the supported tags
+ *
  * @param {string} name Name to test
  * @param {readonly string[]} supported Supported tags
- * @return {boolean}
+ * @returns {boolean}
  */
 function isSupportedIdentifier(
-  name: string,
-  supported: readonly string[],
+	name: string,
+	supported: readonly string[],
 ): boolean {
-  for (const supportedName of supported) {
-    if (supportedName === name) {
-      return true;
-    }
-  }
+	for (const supportedName of supported) {
+		if (supportedName === name) {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 /**
  * Extracts stylesheets from a given source string
+ *
  * @param {string} source Source to parse
  * @param {SyntaxOptions} options Syntax options
- * @return {Set<NodePath<CallExpression>>}
+ * @returns {Set<NodePath<CallExpression>>}
  */
 export function extractCallExprFromSource(
-  source: string,
-  options: SyntaxOptions,
+	source: string,
+	options: SyntaxOptions,
 ): Set<NodePath<CallExpression>> {
-  const extractedStyles = new Set<NodePath<CallExpression>>();
-  const names = options?.names;
-  if (!names) {
-    return extractedStyles;
-  }
-  const ast = babelParse(source, {
-    sourceType: "unambiguous",
-    plugins: ["typescript"],
-    ranges: true,
-  });
-  traverse(ast, {
-    CallExpression: (path: NodePath<CallExpression>): void => {
-      const calleeNode = path.get("callee");
-      if (calleeNode.node.start !== null && calleeNode.node.end !== null) {
-        const name = calleeNode.toString();
-        if (
-          isSupportedIdentifier(name, names) &&
-          !hasDisableComment(path, options)
-        ) {
-          extractedStyles.add(path);
-        }
-      }
-    },
-  });
+	const extractedStyles = new Set<NodePath<CallExpression>>();
+	const names = options?.names;
+	if (!names) {
+		return extractedStyles;
+	}
+	const ast = babelParse(source, {
+		sourceType: "unambiguous",
+		plugins: ["typescript"],
+		ranges: true,
+	});
+	traverse(ast, {
+		CallExpression: (path: NodePath<CallExpression>): void => {
+			const calleeNode = path.get("callee");
+			if (calleeNode.node.start !== null && calleeNode.node.end !== null) {
+				const name = calleeNode.toString();
+				if (
+					isSupportedIdentifier(name, names) &&
+					!hasDisableComment(path, options)
+				) {
+					extractedStyles.add(path);
+				}
+			}
+		},
+	});
 
-  return extractedStyles;
+	return extractedStyles;
 }
 
 if (import.meta.vitest) {
-  const { dedent } = await import("@qnighy/dedent");
-  const { describe, it, expect } = import.meta.vitest;
+	const { dedent } = await import("@qnighy/dedent");
+	const { describe, it, expect } = import.meta.vitest;
 
-  describe("extract", () => {
-    describe("extractCallExprFromSource", () => {
-      it("should be empty if no tag names (default)", () => {
-        const result = extractCallExprFromSource("", {
-          id: "foo",
-        });
+	describe("extract", () => {
+		describe("extractCallExprFromSource", () => {
+			it("should be empty if no tag names (default)", () => {
+				const result = extractCallExprFromSource("", {
+					id: "foo",
+				});
 
-        expect(result.size).toBe(0);
-      });
-      it("should detect call expr", () => {
-        const source = dedent`\
+				expect(result.size).toBe(0);
+			});
+			it("should detect call expr", () => {
+				const source = dedent`\
           const scopedVar = createVar();
           export const flexContainer = style({
             display: 'flex'
           });
         `;
-        const result = extractCallExprFromSource(source, {
-          id: "foo",
-          names: ["style"],
-        });
+				const result = extractCallExprFromSource(source, {
+					id: "foo",
+					names: ["style"],
+				});
 
-        expect(result.size).toBe(1);
-      });
-      it("should parse typescript", () => {
-        const source = dedent`\
+				expect(result.size).toBe(1);
+			});
+			it("should parse typescript", () => {
+				const source = dedent`\
           export const accentVar: CSSVarFunction = createVar();
 
           export const blue = style({
@@ -100,27 +102,27 @@ if (import.meta.vitest) {
             }
           });
         `;
-        const result = extractCallExprFromSource(source, {
-          id: "foo",
-          names: ["style"],
-        });
+				const result = extractCallExprFromSource(source, {
+					id: "foo",
+					names: ["style"],
+				});
 
-        expect(result.size).toBe(1);
-      });
-      it("should respect disable comments", () => {
-        const source = dedent`\
+				expect(result.size).toBe(1);
+			});
+			it("should respect disable comments", () => {
+				const source = dedent`\
           // postcss-foo-disable-next-line
           export const flexContainer = style({
             display: 'flex'
           });
         `;
-        const result = extractCallExprFromSource(source, {
-          id: "foo",
-          names: ["style"],
-        });
+				const result = extractCallExprFromSource(source, {
+					id: "foo",
+					names: ["style"],
+				});
 
-        expect(result.size).toBe(0);
-      });
-    });
-  });
+				expect(result.size).toBe(0);
+			});
+		});
+	});
 }
